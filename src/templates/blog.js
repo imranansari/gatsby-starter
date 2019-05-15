95e12c974a04b6177b575521307e139248a13271
jabedford <32302360+JABedford@users.noreply.github.com>
2019-05-15 08:29:46 +0100

    Query for blog posts

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,17 +1,35 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout'
 
-const blog = () => {
+export const query = graphql`
+  query ($slug: String!) {
+    markdownRemark ( fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const blog = props => {
   return (
     <Layout>
         <section>
-            <div className="column-left">
-                 Blog Post
-            </div>
+            <div className="column-left"></div>
             <div className="column-main">
-                <h1 className="column-main-container-large">Hello</h1>
+                <h1 className="column-main-container-large">
+                  { props.data.markdownRemark.frontmatter.title }
+                </h1>
+                <h5>
+                  { props.data.markdownRemark.frontmatter.date }
+                </h5>
+                <div className="post-content">
+                  <div dangerouslySetInnerHTML={{__html: props.data.markdownRemark.html}}></div>
+                </div>
             </div>
         </section>  
     </Layout>
