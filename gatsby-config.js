41196612901feeb5d592a3fc576342b176274608
jabedford <32302360+JABedford@users.noreply.github.com>
2019-05-15 06:46:26 +0100

    source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
     name: 'James Bedford',
     jobtitle: 'Front End Developer',
     email: 'hello@jamesbedford.com'
-  }
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    }  
+  ]
 }
